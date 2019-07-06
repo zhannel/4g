63aee320935c63bfba4eb929181e611ba1220931
Zhannel Ikhsanova-Solod <zhannel0804@gmail.com>
2019-07-06 20:38:59 +0600

    privetuli

--- bot.py
+++ bot.py
@@ -1,20 +1,18 @@
-# Настройки
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-updater = Updater(token='848577923:AAHlsUHjNBWB6QprMt9HSQ79ZA9HJLvgssU') # Токен API к Telegram
-dispatcher = updater.dispatcher
-# Обработка команд
-def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Hello, niga')
-def textMessage(bot, update):
-    response = 'Niga wassup: ' + update.message.text
-    bot.send_message(chat_id=update.message.chat_id, text=response)
-# Хендлеры
-start_command_handler = CommandHandler('start', startCommand)
-text_message_handler = MessageHandler(Filters.text, textMessage)
-# Добавляем хендлеры в диспетчер
-dispatcher.add_handler(start_command_handler)
-dispatcher.add_handler(text_message_handler)
-# Начинаем поиск обновлений
-updater.start_polling(clean=True)
-# Останавливаем бота, если были нажаты Ctrl + C
-updater.idle()
+import telebot;
+
+
+import telepot
+import urllib3
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3,maxsize=10, retries=False, timeout=30),}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+
+bot = telebot.TeleBot('%887578648:AAFIGPEzk31SkgqIXp1cjtaJRbKNKQtFg04%');
+@bot.message_handler(content_types=['text', 'document', 'audio'])
+def get_text_messages(message):
+    if message.text == "Hello":
+        bot.send_message(message.from_user.id, "Hello, can i help you?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Write Hello")
+    else:
+        bot.send_message(message.from_user.id, "I can't udestand you. Write /help")
