89604f354f52f96e07acd1ffc106941531227732
zhannel <39848644+zhannel@users.noreply.github.com>
2019-07-07 05:22:23 +0600

    Create IFileManager.cs

---
+++ NewFolder/Data/FileManager/IFileManager.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neupusti.Data.FileManager
+{
+    public interface IFileManager
+    {
+        FileStream ImageStream(string image);
+        Task<string> SaveImage(IFormFile image);
+        bool RemoveImage(string image);
+    }
+}
