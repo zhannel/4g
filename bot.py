a06b1d3f12ac309310530ce1c24c90a8e41c6dab
altmurzilka <42155991+altmurzilka@users.noreply.github.com>
2019-07-07 01:08:44 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,3 +1,50 @@
+<<<<<<< HEAD
+import requests
+import misc #тут токен
+# import json #to output russian text properly
+# https://api.telegram.org/bot848577923:AAHlsUHjNBWB6QprMt9HSQ79ZA9HJLvgssU/sendmessage?chat_id=389181251&text=pidor
+
+token = misc.token
+URL = 'https://api.telegram.org/bot' + token + '/'
+
+def get_updates():
+    url = URL + 'getupdates'
+    r = requests.get(url)
+    return r.json()
+
+
+
+
+def get_message():
+    data = get_updates()
+    chat_id = data['result'][-1]['message']['chat']['id']
+    message_text = data['result'][-1]['message']['text']
+
+    message = {'chat_id': chat_id,
+               'text': message_text}
+    return message
+
+
+def send_message(chat_id, text='go kill tourself'):
+    url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
+    requests.get(url)
+    
+def main():
+    answer = get_message()
+    chat_id = answer['chat_id']
+    text = answer['text']
+
+    if 'sam pidor' in text:
+        send_message(chat_id, 'pidora otvet')
+   
+
+if __name__ == '__main__':
+    main()
+
+
+    
+
+=======
 import requests
 import misc #тут токен
 import json #to output russian text properly
@@ -44,3 +91,4 @@ if __name__ == '__main__':
 
     
 
+>>>>>>> 0c640e4b8dd3aa354071250fb55297c6648e13cb
