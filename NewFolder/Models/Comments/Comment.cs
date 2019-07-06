78a047fc098248cabd3111e2a01c1ca2c65779d7
apalibaeva <38186235+apalibaeva@users.noreply.github.com>
2019-07-07 05:30:46 +0600

    Add files via upload

--- NewFolder/Models/Comments/Comment.cs
+++ NewFolder/Models/Comments/Comment.cs
@@ -1,1 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace Neupusti.Models.Comments
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
