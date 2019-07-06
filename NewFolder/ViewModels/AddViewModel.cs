0f5b329d0ca6796ea3f7792fe6a86161bc3ce8db
apalibaeva <38186235+apalibaeva@users.noreply.github.com>
2019-07-07 05:33:02 +0600

    Add files via upload

--- NewFolder/ViewModels/AddViewModel.cs
+++ NewFolder/ViewModels/AddViewModel.cs
@@ -1,1 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace Neupusti.ViewModels
+{
+    public class AddViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Body { get; set; } = "";
+    }
+}
---
+++ NewFolder/ViewModels/CommentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neupusti.ViewModels
+{
+    public class CommentViewModel
+    {
+        [Required]
+        public int PostId { get; set; }
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
