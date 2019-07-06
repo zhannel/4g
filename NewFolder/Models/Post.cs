d1d72723f4603cea81199846a8295107967cdd5f
apalibaeva <38186235+apalibaeva@users.noreply.github.com>
2019-07-07 05:29:45 +0600

    Add files via upload

---
+++ NewFolder/Models/Post.cs
@@ -0,0 +1,24 @@
+﻿using Neupusti.Models.Comments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neupusti.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Body { get; set; } = "";
+        public string Image { get; set; } = "";
+        public string Premium { get; set; }
+        public string Description { get; set; } = "";
+        public string Tags { get; set; } = "";
+        public string Category { get; set; } = "";
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public List<Comment> Comments { get; set; }
+    }
+}
