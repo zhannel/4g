d749bc172d185e12f003b75e471dfd4940265d75
apalibaeva <38186235+apalibaeva@users.noreply.github.com>
2019-07-07 05:15:32 +0600

    Add files via upload

--- Hackaton/Controllers/AddController.cs
+++ Hackaton/Controllers/AddController.cs
@@ -1,1 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Neupusti.Data.FileManager;
+using Neupusti.Data.Repository;
+using Neupusti.Models;
+using Neupusti.ViewModels;
 
+namespace Neupusti.Controllers
+{
+    [Authorize(Roles = "User")]
+    public class AddController : Controller
+    {
+
+        private IRepository _repo;
+        private IFileManager _fileManager;
+
+        public AddController(IRepository repo, IFileManager fileManager)
+        {
+            _repo = repo;
+            _fileManager = fileManager;
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return View(new AddViewModel());
+            }
+            else
+            {
+                var post = _repo.GetPost((int)id);
+                return View(new AddViewModel
+                {
+                    Id = post.Id,
+                    Title = post.Title,
+                    Body = post.Body
+                });
+            }
+
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AddViewModel vm)
+        {
+            var post = new Post
+            {
+                Id = vm.Id,
+                Title = vm.Title,
+                Body = vm.Body
+            };
+            
+            if (post.Id > 0)
+            {
+                _repo.UpdatePost(post);
+            }
+            else
+                _repo.AddPost(post);
+
+            if (await _repo.SaveChangesAsync())
+                return RedirectToAction("/Home/Index");
+            else
+                return View(post);
+        }
+    }
+}
