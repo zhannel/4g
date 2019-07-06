debde0309356d2062923833509288ec493964b86
apalibaeva <38186235+apalibaeva@users.noreply.github.com>
2019-07-07 05:28:48 +0600

    Add files via upload

--- NewFolder/Models/User.cs
+++ NewFolder/Models/User.cs
@@ -1,1 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace Neupusti.Models
+{
+    public class User : IdentityUser
+    {
+    }
+}
