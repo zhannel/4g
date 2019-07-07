665dcd37f740a5c7f94881cb806d00803fd4bb64
altmurzilka <42155991+altmurzilka@users.noreply.github.com>
2019-07-07 07:07:56 +0600

    Add files via upload

---
+++ NewFolder/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Neupusti.Data;
+using Neupusti.Models;
+
+namespace Neupusti
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            try
+            { 
+                var scope = host.Services.CreateScope();
+                var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+                var roleMgr = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                ctx.Database.EnsureCreated();
+
+                var userRole = new IdentityRole("User");
+                var premiumRole = new IdentityRole("PremiumUser");
+                var adminRole = new IdentityRole("Admin");
+                if(!ctx.Roles.Any())
+                {
+                    roleMgr.CreateAsync(adminRole).GetAwaiter().GetResult();
+                    roleMgr.CreateAsync(premiumRole).GetAwaiter().GetResult();
+                    roleMgr.CreateAsync(userRole).GetAwaiter().GetResult();
+                    //create a role
+                }
+
+                if(!ctx.Users.Any(u => u.UserName=="admin"))
+                {
+                    var adminUser = new User
+                    {
+                        UserName = "admin",
+                        Email = "admin@test.com"
+                    };
+
+                    var result = userMgr.CreateAsync(adminUser, "password").GetAwaiter().GetResult();
+                    userMgr.AddToRoleAsync(adminUser, adminRole.Name).GetAwaiter().GetResult();
+                    //create a role
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ NewFolder/Startup.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Neupusti.Data;
+using Microsoft.EntityFrameworkCore;
+using Neupusti.Data.Repository;
+using Microsoft.AspNetCore.Identity;
+using Neupusti.Data.FileManager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Neupusti
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=App;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<AppDbContext>
+                (options => options.UseSqlServer(connection));
+
+        
+
+
+            services.AddIdentity<Neupusti.Models.User, IdentityRole>(options => 
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Auth/Login";
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminAccess", policy => policy.RequireRole("Admin"));
+                options.AddPolicy("PremiumUSerAccess", policy =>
+                    policy.RequireAssertion(context => context.User.IsInRole("Admin")
+                    || context.User.IsInRole("PremiumUser")));
+                options.AddPolicy("UserAccess", policy =>
+                    policy.RequireAssertion(context => context.User.IsInRole("Admin")
+                    || context.User.IsInRole("PremiumUser")
+                    || context.User.IsInRole("User")));
+            }
+                );
+
+            services.AddMvc(options =>
+                options.CacheProfiles.Add("Monthly", new CacheProfile { Duration = 60 * 60 * 24 * 7 * 4 })
+                );
+
+            services.AddTransient<IRepository, Repository>();
+
+            services.AddTransient<IFileManager, FileManager>();
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
